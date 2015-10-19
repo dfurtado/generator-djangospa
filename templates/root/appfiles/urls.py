a1ce7d26a4b9fe04811f29ed08100d87f08ace06
Daniel Furtado <daniel@dfurtado.com>
2015-10-19 19:33:00 +0200

    Added routes for login and logout

--- templates/root/appfiles/urls.py
+++ templates/root/appfiles/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
 	url(r'^$', '<%= appName %>.views.index', name="index"),
     url(r'^admin/', include(admin.site.urls)),
-]
+)
+
+urlpatterns += patterns(
+    'django.contrib.auth',
+    url(r'^accounts/login/$', 'views.login', { 'template_name': 'app/login.html'}),
+    url(r'^accounts/logout/$', 'views.logout', { 'next_page': '/'}),
+)
