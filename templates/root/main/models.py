69f209cc44dd7391ace040f1604fd599ddf3d9d4
Pete <Nolapete@users.noreply.github.com>
2015-11-05 15:10:20 -0600

    Update models.py
    Add Sample model for Django REST Framework

--- templates/root/main/models.py
+++ templates/root/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Sample(models.Model):
+	created = models.DateTimeField(auto_now_add=True)
+	name = models.CharField(max_length=150, blank=True, default='')
+	img_name = models.CharField(max_length=100, blank=True, default='')
+	info = models.TextField()	
+	owner = models.ForeignKey('auth.user', related_name='sample')
+	
+	class Meta:
+		ordering = ('created',)
+
+	def __unicode__(self):
+		return self.name
+		
