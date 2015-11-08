d962a93e740eaf399f1e44a1b0679134ad842d79
Daniel Furtado <daniel@dfurtado.com>
2015-11-08 20:01:35 +0100

    Added gulp file

---
+++ templates/root/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+	path = require('path');
+
+var paths = {
+
+	jslibs: [
+		'bower_components/angular/angular.min.js', 
+        'bower_components/angular-route/angular-route.min.js',
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'bower_components/jquery/dist/jquery.min.js'
+	],
+
+	styles: [
+		'bower_components/bootstrap/dist/css/bootstrap.min.css'
+	],
+
+	fonts: [
+		'bower_components/bootstrap/fonts/**/*'
+	],
+	
+	dest: {
+		scripts:   path.join(__dirname, 'static', 'scripts'),
+		styles:    path.join(__dirname, 'static', 'styles')
+	}
+}
+
+gulp.task('copy-styles', function() {
+	gulp.src(paths.styles)
+		.pipe(gulp.dest(path.join(paths.dest.styles, 'css')));
+
+	gulp.src(paths.fonts)
+		.pipe(gulp.dest(path.join(paths.dest.styles, 'fonts')));
+
+});
+
+gulp.task('copy-scripts', function() {
+	gulp.src(paths.jslibs)
+		.pipe(gulp.dest(paths.dest.scripts));
+});
+
+gulp.task('default', ['copy-scripts', 'copy-styles'])
