a526f798aa6cf695c5be219aab68107e57d1f209
Nolapete <fish4pete@gmail.com>
2015-11-28 13:40:25 -0600

    Update admin.py for models to show in admin

--- templates/root/main/admin.py
+++ templates/root/main/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from .models import Sample, Page, Item
+
+class SampleAdmin(admin.ModelAdmin):
+    pass
+
+class PageAdmin(admin.ModelAdmin):
+    pass
+
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Sample, SampleAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(Item, ItemAdmin)
