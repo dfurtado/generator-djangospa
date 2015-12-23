f65bcfb345446bafe219fa93710392119a85e652
Daniel Furtado <daniel@dfurtado.com>
2015-12-23 01:30:14 +0100

    more code cleanup

--- templates/root/main/permissions.py
+++ templates/root/main/permissions.py
@@ -1,15 +1,15 @@
 from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-	"""
-	Custom permission to only allow owners of an object to edit it.
-	"""
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
 
-	def has_object_permissions(self, request, view, obj):
-		# Read permissions are allowed to any request,
-		# so we'll always allow GET, HEAD, or OPTIONS requests.
-		if request.method in permissions.SAFE_METHODS:
-			return True
+    def has_object_permissions(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD, or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
 
-		# Write permissions are only allowed to the owner of the snippet.
-		return obj.owner == request.user
+        # Write permissions are only allowed to the owner of the snippet.
+         return obj.owner == request.user
